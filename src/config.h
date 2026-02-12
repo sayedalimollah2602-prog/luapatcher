d0d301c61c7534acd1b1728af2f46a6597d62d4d
sayedalimollah2602-prog <sayedalimollah2602@gmail.com>
2026-02-12 23:17:00 +0530

    release:  1.3.0

--- src/config.h
+++ src/config.h
@@ -7,7 +7,7 @@
 #include <QFileInfo>
 
 namespace Config {
-    const QString APP_VERSION = "1.2.1";
+    const QString APP_VERSION = "1.3.0";
     const QString WEBSERVER_BASE_URL = "https://webserver-ecru.vercel.app";
     
     // Server access token - check local file first, then macro
